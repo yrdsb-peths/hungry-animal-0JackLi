973f5dd7bd85a993d028af5a85666a6ae0813cee
Jack Li <150150571+0JackLi@users.noreply.github.com>
2023-11-22 09:07:01 -0500

    made apple fall

--- MyWorld.java
+++ MyWorld.java
@@ -18,9 +18,17 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         Background background = new Background(); 
-        Apple apple = new Apple();
         Elephant elephant = new Elephant();
         addObject(background,100, 150);
         addObject(elephant, 300, 300);
+        spawnApple();
+    }
+    
+    public void spawnApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
